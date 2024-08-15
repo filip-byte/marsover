197fb23e9b4d8c48e6105c7e41ef6e000c93b943
filip-byte <ohboytoby@gmail.com>
2024-08-15 15:34:27 +0100

    Feat: Add plateauSize and Position getters and setters

--- src/main/java/Input/Position.java
+++ src/main/java/Input/Position.java
@@ -4,4 +4,34 @@ public class Position {
     private int x;
     private int y;
     private Directions facing;
+
+    public Position(int x, int y, Directions facing) {
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Directions getFacing() {
+        return facing;
+    }
+
+    public void setFacing(Directions facing) {
+        this.facing = facing;
+    }
 }
