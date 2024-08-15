36c2814fd84ed819fd33460c0f1bc0b00e08a9d8
filip-byte <ohboytoby@gmail.com>
2024-08-15 23:50:19 +0100

    test: Add comprehensive tests for parseInstructions method

--- src/main/java/Input/Parsers/InputParser.java
+++ src/main/java/Input/Parsers/InputParser.java
@@ -4,8 +4,10 @@ import Data.Plateau;
 import Input.Direction;
 import Input.Instruction;
 import Input.Position;
+import com.sun.source.tree.LiteralTree;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class InputParser {
 
@@ -14,7 +16,6 @@ public class InputParser {
         if (input == null || input.length() > 5 || input.trim().isEmpty() || !input.matches("^\\d{1,2}\\s\\d{1,2}$")) {
             throw new IllegalArgumentException("Please provide a valid input. Maximum size of the plateau is 99 x 99");
         }
-
 
         int[] array = Arrays.stream(input.split(" "))
                 .mapToInt(Integer::parseInt)
@@ -29,12 +30,32 @@ public class InputParser {
 
     public static Position parseStartingPosition (String input) {
 
-        return new Position(1, 2, Direction.N);
+        if (input == null || input.length() > 7 || input.trim().isEmpty() || !input.matches("^\\d{1,2}\\s\\d{1,2}\\s[NSEW]$")) {
+            throw new IllegalArgumentException("Please provide a valid input, e.g. 10 10 E");
+        }
+
+        String[] array = input.split(" ");
+
+        int x = Integer.valueOf(array[0]);
+        int y = Integer.valueOf(array[1]);
+        Direction facing = Direction.valueOf(array[2]);
+
+        return new Position(x, y, facing);
     }
 
     public static Instruction[] parseInstructions (String input) {
 
-        return new Instruction[0];
+        if (input == null || input.length() > 15 || input.length() == 0 || input.trim().isEmpty() || !input.matches("^[LRM]{1,15}$")) {
+            throw new IllegalArgumentException("Please provide a valid input, max 15 instructions.");
+        }
+
+        String [] array = input.split("");
+
+        Instruction [] instructions = Arrays.stream(array)
+                .map(s -> Instruction.valueOf(s))
+                .toArray(Instruction[]::new);
+
+        return instructions;
     }
 
 
--- src/test/java/Input/Parsers/InputParserTest.java
+++ src/test/java/Input/Parsers/InputParserTest.java
@@ -1,6 +1,9 @@
 package Input.Parsers;
 
 import Data.Plateau;
+import Input.Direction;
+import Input.Instruction;
+import Input.Position;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 
@@ -54,18 +57,92 @@ class InputParserTest {
 
     }
 
+    @Test
+    @Description("Method returns a Position for valid input")
+    void parseStartingPosition() {
 
+        // Arrange
+        String validInput = "12 15 N";
+        Position expectedOutcome = new Position(12, 15, Direction.N);
 
+        String validInput2 = "85 43 W";
+        Position expectedOutcome2 = new Position(85, 43, Direction.W);
 
+        //Act
+        Position result = InputParser.parseStartingPosition(validInput);
+        Position result2 = InputParser.parseStartingPosition(validInput2);
 
+        //Assert
+        assertAll(
+                () -> assertEquals(expectedOutcome.getFacing(), result.getFacing()),
+                () -> assertEquals(expectedOutcome.getX(), result.getX()),
+                () -> assertEquals(expectedOutcome.getY(), result.getY())
+        );
 
+        assertAll(
+                () -> assertEquals(expectedOutcome2.getFacing(), result2.getFacing()),
+                () -> assertEquals(expectedOutcome2.getX(), result2.getX()),
+                () -> assertEquals(expectedOutcome2.getY(), result2.getY())
+        );
+    }
 
+        @Test
+        @Description("Method handles invalid input")
+        void parseStartingPosition_Test_Invalid_input() {
+            //Arrange
+            String oneNumberOnly = "1234 N";
+            String emptyString = " ";
+            String nullString = null;
+            String tooLong = "123 456 N";
+            String incorrectFormat = "22 X 22 N";
+            String incorrectFormat2 = "22 22 NW";
+
+            var expectedOutcome = IllegalArgumentException.class;
+
+            //Act & Assert
+            assertThrows(expectedOutcome, () -> InputParser.parseStartingPosition(oneNumberOnly));
+            assertThrows(expectedOutcome, () -> InputParser.parseStartingPosition(emptyString));
+            assertThrows(expectedOutcome, () -> InputParser.parseStartingPosition(nullString));
+            assertThrows(expectedOutcome, () -> InputParser.parseStartingPosition(tooLong));
+            assertThrows(expectedOutcome, () -> InputParser.parseStartingPosition(incorrectFormat));
+            assertThrows(expectedOutcome, () -> InputParser.parseStartingPosition(incorrectFormat2));
+
+        }
 
     @Test
-    void parseStartingPosition() {
+    void parseInstructions_With_Valid_Input() {
+
+        String inputWithDigits = "LRM123LRM";
+        String emptyString = " ";
+        String nullString = null;
+        String tooLong = "LRMLRMLRMLRMLRML";
+        String incorrectFormat = "L R M";
+        String incorrectFormat2 = "123";
+
+        var expectedOutcome = IllegalArgumentException.class;
+
+        //Act & Assert
+        assertThrows(expectedOutcome, () -> InputParser.parseInstructions(inputWithDigits));
+        assertThrows(expectedOutcome, () -> InputParser.parseInstructions(emptyString));
+        assertThrows(expectedOutcome, () -> InputParser.parseInstructions(nullString));
+        assertThrows(expectedOutcome, () -> InputParser.parseInstructions(tooLong));
+        assertThrows(expectedOutcome, () -> InputParser.parseInstructions(incorrectFormat));
+        assertThrows(expectedOutcome, () -> InputParser.parseInstructions(incorrectFormat2));
+
     }
 
     @Test
-    void parseInstructions() {
+    @Description("Returns an array of Instructions when valid input provided.")
+    void parseInstructionsTest() {
+        String input = "LMRM";
+        Instruction []  expectedOutcome = new Instruction [] {Instruction.L, Instruction.M, Instruction.R, Instruction.M};
+
+        var result = InputParser.parseInstructions(input);
+
+        assertEquals(expectedOutcome.length, result.length);
+        assertEquals(expectedOutcome[0], result[0]);
+        assertEquals(expectedOutcome[1], result[1]);
+        assertEquals(expectedOutcome[2], result[2]);
+        assertEquals(expectedOutcome[3], result[3]);
     }
 }
